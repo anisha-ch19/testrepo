866b7a932d3ad2f9203ac6126a42700b8cf8f8e4
anisha-ch19 <chourasiaanisha4@gmail.com>
2023-10-05 14:46:10 +0530

    new code 299

--- login.java
+++ login.java
@@ -8,9 +8,21 @@ division
 
 ........
 
-////////
-//
-//
-//
-//
-//
+public class login ()
+{
+	public static void main (string args [])
+	
+	{
+		int a =30;
+
+		for(i=0;i<=10;i++)
+
+		{
+			system.out.println("print : " i);
+
+		 }
+
+        }
+
+}
+
