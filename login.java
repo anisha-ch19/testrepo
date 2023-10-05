62ec9ac2bb4edc0435b283daec1ef4be39e81bbd
anisha-ch19 <chourasiaanisha4@gmail.com>
2023-10-05 14:52:44 +0530

    some code

--- login.java
+++ login.java
@@ -8,9 +8,18 @@ division
 
 ........
 
-////////
-//
-//
-//
-//
-//
+public class login ()
+{
+	public static void main(string args[])
+	{
+		int a =10;
+
+		for(i=0;i<=10;i++)
+
+                  {
+			  system.out.println("print : " i);
+	          }
+	}
+
+}
+
